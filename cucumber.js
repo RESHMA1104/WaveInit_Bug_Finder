d75825c48af40c456ce81a7f74ae0aa76012cd5b
14-Prasanna <2k22cse115@kiot.ac.in>
2026-08-25 22:27:51 +0530

    Removed the allure

--- cucumber.js
+++ cucumber.js
@@ -1,71 +1,32 @@
 const path = require("node:path");
 
-module.exports = {
-    default: {
-        formatOptions: {
-            snippetInterface: "async-await"
-        },
-
-        requireModule: [
-            "ts-node/register"
+module.exports={
+    default:{
+        formatoptions:{snippetInterface:"async-wait"},
+        requireModule:['ts-node/register'],
+        require:['src/test/hooks/**/*.ts',
+            'src/test/stepdefinitions/**/*.ts',
+            'src/test/world/**/*.ts'
         ],
-
-        require: [
-            "src/test/hooks/**/*.ts",
-            "src/test/stepdefinitions/**/*.ts",
-            "src/test/world/**/*.ts"
-        ],
-
-        paths: [
-            "src/test/features/**/*.feature"
-        ],
-
-        publishQuiet: true,
-        dryRun: false,
-
-        format: [
-            "snippets",
-            "progress",
-
-            "json:reports/cucumber-json/cucumber-report.json",
-
-            "html:reports/cucumber-html/cucumber-report.html",
-
-            "rerun:rerun/@rerun.txt",
-
-            "allure-cucumberjs/reporter:allure-results"
-        ],
-
-        parallel: 1
+        paths:['src/test/features/**/*.feature'],
+        publishQuiet:true,
+        dryRun:false,
+        format:['snippets','progress','json:reports/cucumber-json/cucumber-report.json',
+        'html:reports/cucumber-html/cucumber-report.html',
+        'rerun:rerun/@rerun.txt'],
+        parallel:1
     },
 
-    rerun: {
-        requireModule: [
-            "ts-node/register"
+    rerun:{
+        requireModule:['ts-node/register'],
+        require:['src/test/hooks/**/*.ts',
+            'src/test/stepdefinitions/**/*.ts',
+            'src/test/world/**/*.ts'
         ],
-
-        require: [
-            "src/test/hooks/**/*.ts",
-            "src/test/stepdefinitions/**/*.ts",
-            "src/test/world/**/*.ts"
-        ],
-
-        paths: [
-            "rerun/@rerun.txt"
-        ],
-
-        format: [
-            "snippets",
-
-            "json:reports/cucumber-json/cucumber-report.json",
-
-            "html:reports/cucumber-html/cucumber-report.html",
-
-            "rerun:rerun/@rerun.txt",
-
-            "allure-cucumberjs/reporter:allure-results"
-        ],
-
-        parallel: 1
+        paths:['rerun/@rerun.txt'],
+        format:['snippets','json:reports/cucumber-json/cucumber-report.json',
+            'html:reports/cucumber-html/cucumber-report.html',
+            'rerun:rerun/@rerun.txt'],
+         parallel:1
     }
-};
+}
