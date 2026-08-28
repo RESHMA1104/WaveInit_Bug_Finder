b8082b240b3c5aa0f8b13c92f7fbec71020331b6
Rishwanth Adhishwar <krishwanthadhiswar@gmail.com>
2026-08-28 11:02:28 +0530

    Added 5 scenarios in My Course Feature

--- cucumber.js
+++ cucumber.js
@@ -1,49 +1,54 @@
-const path = require("node:path");
-
 module.exports = {
     default: {
-        formatoptions: { snippetInterface: "async-wait" },
-        requireModule: ['ts-node/register'],
-        require: [
-            'src/test/hooks/**/*.ts',
-            'src/test/stepdefinitions/**/*.ts',
-            'src/test/world/**/*.ts'
-        ],
-        paths: ['src/test/features/**/*.feature'],
-        publishQuiet: true,
-        dryRun: false,
+      formatOptions: {
+        snippetInterface: "async-await"
+      },
+  
+      requireModule: ["ts-node/register"],
+  
+      require: [
+        'src/test/hooks/**/*.ts', 
+        'src/test/stepdefinitions/**/*.ts',
+        'src/test/world/**/*.ts'
+      ],
+  
+      paths: ['src/test/features/**/*.feature'],
+  
+      publishQuiet: true,
+      dryRun: false,
+  
         format: [
-            'snippets',
-            'progress',
-            'json:reports/cucumber-json/cucumber-report.json',
-            'html:reports/cucumber-html/cucumber-report.html',
-            'allure-cucumberjs/reporter',
-            'rerun:rerun/@rerun.txt'
+          'snippets',
+          'progress',
+          'json:reports/cucumber-json/cucumber-report.json',
+          'html:reports/cucumber-html/cucumber-report.html',
+          //'allure-cucumberjs/reporter',
+          'rerun:rerun/@rerun.txt'
         ],
-        formatOptions: {
-            resultsDir: 'allure-results'
-        },
-        parallel: 1
+  
+      parallel: 1
     },
-
+  
     rerun: {
-        requireModule: ['ts-node/register'],
-        require: [
-            'src/test/hooks/**/*.ts',
-            'src/test/stepdefinitions/**/*.ts',
-            'src/test/world/**/*.ts'
-        ],
-        paths: ['rerun/@rerun.txt'],
-        format: [
-            'snippets',
-            'json:reports/cucumber-json/cucumber-report.json',
-            'html:reports/cucumber-html/cucumber-report.html',
-            'allure-cucumberjs/reporter',
-            'rerun:rerun/@rerun.txt'
-        ],
-        formatOptions: {
-            resultsDir: 'allure-results'
-        },
-        parallel: 1
-    }
-};
+      requireModule: ["ts-node/register"],
+  
+      require: [
+        'src/test/hooks/**/*.ts', 
+        'src/test/stepdefinitions/**/*.ts',
+        'src/test/world/**/*.ts'
+      ],
+  
+      paths: ['rerun/@rerun.txt'],
+  
+      format: [
+        'progress',
+        'json:reports/rerun-cucumber-json/cucumber-report.json',
+        'html:reports/rerun-cucumber-html/cucumber-report.html',
+        //'allure-cucumberjs/reporter',
+        'rerun:rerun/@rerun.txt'
+      ],
+  
+      parallel: 1
+    } 
+  };
+  
--- src/test/features/learnerFunctionalityFeatures/learnerMyCourse.feature
+++ src/test/features/learnerFunctionalityFeatures/learnerMyCourse.feature
@@ -12,8 +12,38 @@ Feature: Rishwanth_Adhishwar_K_27_08_2026 To Check the MyCourse Fuctionalities o
     And the learner clicks on myCourse in left drawer
     Then the learner should redirected to My course page
 
-  @MyCourse
   Scenario: To verify the Courses redirects to Specific Course page
     And the learner clicks on myCourse in left drawer
     And the learner clicks on first course in my Course
     Then the learner should be redirected to specific course
+
+  Scenario: To verify the search functionality in My Courses with valid course
+    And the learner clicks on myCourse in left drawer
+    And the learner enter the valid course in search Bar
+    Then the learner should see results based on searched Course
+
+  Scenario: To verify the search functionality in My Courses with invalid course
+    And the learner clicks on myCourse in left drawer
+    And the learner enter the invalid course in search Bar
+    Then the learner should displayed with No course found text
+
+  @MyCourse
+  Scenario: To verify that lesson tab in specific course redirects to its sub tab
+    And the learner clicks on myCourse in left drawer
+    And the learner clicks on first course in my Course
+    And the learner clicks on lessons tab
+    Then the learner should redirected to learning content sub tab
+
+  @MyCourse
+  Scenario: To verify that AI-Quiz tab in specific course redirects to its sub tab
+    And the learner clicks on myCourse in left drawer
+    And the learner clicks on first course in my Course
+    And the learner clicks on quiz tab
+    Then the learner should redirected to Quizzes  sub tab
+
+  @MyCourse
+  Scenario: To verify that Discussion tab in specific course redirects to its sub tab
+    And the learner clicks on myCourse in left drawer
+    And the learner clicks on first course in my Course
+    And the learner clicks on discussion tab
+    Then the learner should redirected to join discussion sub tab
